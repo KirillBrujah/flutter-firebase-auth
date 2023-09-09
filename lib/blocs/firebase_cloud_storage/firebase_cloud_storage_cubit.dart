610581634f4f992136ad5de3d5ba96e6cef0d96c
kirillbrujah <kirillbrujah@gmail.com>
2023-09-09 18:09:11 +0200

    Remove dead and testing code

--- lib/blocs/firebase_cloud_storage/firebase_cloud_storage_cubit.dart
+++ lib/blocs/firebase_cloud_storage/firebase_cloud_storage_cubit.dart
@@ -2,11 +2,9 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:firebase_auth_study/blocs/blocs.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:path_provider/path_provider.dart';
 
 part 'firebase_cloud_storage_state.dart';
 
@@ -43,34 +41,17 @@ class FirebaseCloudStorageCubit extends Cubit<FirebaseCloudStorageState> {
 
     final mountainsRef = storageRef.child("users/$_uid/images/mountains.jps");
 
-    // Directory appDocDir = await getApplicationDocumentsDirectory();
-    // String filep
-
     File file = File(filePath);
     await mountainsRef.putFile(file);
-
-    // try {
-    // } catch (e)
-    // {
-    //   print(e);
-    // }
   }
 
   Future<void> downloadFile() async {
-    // emit(FirebaseCloudStorageState.loaded(
-    //     "https://firebasestorage.googleapis.com/v0/b/fir-study-3ef93.appspot.com/o/users%2F48SqUhPPvKYt4sYZf29I96n877V2%2Fimages%2Fmountains.jps?alt=media&token=ca061929-f4ca-4e2e-8554-a951998d2ec7"));
-    // return;
-    final _uid = "48SqUhPPvKYt4sYZf29I96n877V2";
-
     if (_uid == null) return;
     final storageRef = _storage.ref();
-
-    // print(_uid);
 
     final pathReference = storageRef.child("users/$_uid/images/mountains.jps");
 
     final url = await pathReference.getDownloadURL();
-    print(url);
 
     emit(FirebaseCloudStorageState.loaded(url));
   }
